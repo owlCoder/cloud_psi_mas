6cb7de1793d0400278fdfbeb66e914e0ead53d1f
Danijel Jovanović <danijel.xda@gmail.com>
2024-10-22 20:56:12 +0200

    user

---
+++ Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        public required  string UserId { get; set; }
+        public required string Fullname { get; set; }
+
+        public User()
+        {
+            UserId = Guid.NewGuid().ToString().Replace("-", "");
+        }
+    }
+}
